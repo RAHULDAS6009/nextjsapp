1efd9d566b28724b3248a6e67e3f2a4903c2de66
RAHULDAS6009 <das206053@gmail.com>
2024-10-06 20:30:02 +0530

    db init

--- app/api/v1/user/signup/route.ts
+++ app/api/v1/user/signup/route.ts
@@ -1,13 +1,19 @@
+"use server";
+import { PrismaClient } from "@prisma/client/extension";
 import { NextRequest, NextResponse } from "next/server";
 
+const prisma = new PrismaClient();
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  //save in the database 
-  
+  //save in the database
+  const user = await prisma.user.create({
+    data: {
+      username: body.username,
+      password: body.password,
+    },
+  });
 
   return NextResponse.json({
-    username: body.username,
-    email: body.email,
-    password: body.password,
+    user: user,
   });
 }

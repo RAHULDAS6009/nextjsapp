fe92ed7e15c8611efb8b72b36800e2780ac77c58
RAHULDAS6009 <das206053@gmail.com>
2024-10-07 19:06:59 +0530

    best practices for prisma client code

--- app/api/v1/user/signin/route.ts
+++ app/api/v1/user/signin/route.ts
@@ -1,9 +1,10 @@
 "use server";
 import { NextRequest, NextResponse } from "next/server";
 
+
 export async function GET(req: NextRequest) {
   const body = await req.json();
-  //save in the database
+  
 
   return NextResponse.json({});
 }
--- app/api/v1/user/signup/route.ts
+++ app/api/v1/user/signup/route.ts
@@ -1,19 +1,41 @@
 "use server";
-import { PrismaClient } from "@prisma/client/extension";
+import prisma from "@/db";
 import { NextRequest, NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  //save in the database
-  const user = await prisma.user.create({
-    data: {
-      username: body.username,
-      password: body.password,
-    },
-  });
+  //zod validation
+  try {
+    //save in the database
+    const userExsist = await prisma.user.findUnique({
+      where: {
+        username: body.username,
+      },
+    });
+    //user exsist in the database
+    if (userExsist) {
+      return NextResponse.json({
+        msg: "user exsist in the database",
+      });
+    }
 
-  return NextResponse.json({
-    user: user,
-  });
+    if (body.password.length < 6) {
+      return NextResponse.json({
+        msg: "password must be atleast six characters",
+      });
+    }
+    const user = await prisma.user.create({
+      data: {
+        username: body.username,
+        password: body.password,
+      },
+      //password must be atleast six characters
+      //hash the password
+    });
+    return NextResponse.json({
+      msg: "user saved in the database",
+    });
+  } catch (error) {
+    //user exsist in the database or not
+  }
 }
